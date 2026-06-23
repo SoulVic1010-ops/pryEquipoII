2a64c29aec106ffb93f0ddeff016f6463955b796
Yahir-07 <20251168@uthh.edu.mx>
2026-06-22 20:13:29 -0600

    El inicio

---
+++ pryEquipoII/frmInicio.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace pryEquipoII
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.btnHistorialMedico = new System.Windows.Forms.Button();
+			this.btnConsulta = new System.Windows.Forms.Button();
+			this.btnAgendarCita = new System.Windows.Forms.Button();
+			this.btnRegistroPacientes = new System.Windows.Forms.Button();
+			this.btnInicio = new System.Windows.Forms.Button();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnHistorialMedico
+			// 
+			this.btnHistorialMedico.BackgroundImage = global::pryEquipoII.Properties.Resources.WhatsApp_Image_2026_06_22_at_3_274;
+			this.btnHistorialMedico.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.btnHistorialMedico.Location = new System.Drawing.Point(12, 440);
+			this.btnHistorialMedico.Name = "btnHistorialMedico";
+			this.btnHistorialMedico.Size = new System.Drawing.Size(123, 76);
+			this.btnHistorialMedico.TabIndex = 6;
+			this.btnHistorialMedico.UseVisualStyleBackColor = true;
+			// 
+			// btnConsulta
+			// 
+			this.btnConsulta.BackgroundImage = global::pryEquipoII.Properties.Resources.WhatsApp_Image_2026_06_22_at_3_272;
+			this.btnConsulta.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.btnConsulta.Location = new System.Drawing.Point(13, 358);
+			this.btnConsulta.Name = "btnConsulta";
+			this.btnConsulta.Size = new System.Drawing.Size(123, 76);
+			this.btnConsulta.TabIndex = 5;
+			this.btnConsulta.UseVisualStyleBackColor = true;
+			// 
+			// btnAgendarCita
+			// 
+			this.btnAgendarCita.BackgroundImage = global::pryEquipoII.Properties.Resources.WhatsApp_Image_2026_06_22_at_3_27_54_PM__2_;
+			this.btnAgendarCita.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.btnAgendarCita.Location = new System.Drawing.Point(13, 276);
+			this.btnAgendarCita.Name = "btnAgendarCita";
+			this.btnAgendarCita.Size = new System.Drawing.Size(123, 76);
+			this.btnAgendarCita.TabIndex = 4;
+			this.btnAgendarCita.UseVisualStyleBackColor = true;
+			// 
+			// btnRegistroPacientes
+			// 
+			this.btnRegistroPacientes.BackgroundImage = global::pryEquipoII.Properties.Resources.WhatsApp_Image_2026_06_22_at_3_27_54_PM;
+			this.btnRegistroPacientes.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.btnRegistroPacientes.Location = new System.Drawing.Point(13, 194);
+			this.btnRegistroPacientes.Name = "btnRegistroPacientes";
+			this.btnRegistroPacientes.Size = new System.Drawing.Size(123, 76);
+			this.btnRegistroPacientes.TabIndex = 3;
+			this.btnRegistroPacientes.UseVisualStyleBackColor = true;
+			// 
+			// btnInicio
+			// 
+			this.btnInicio.BackgroundImage = global::pryEquipoII.Properties.Resources.WhatsApp_Image_2026_06_22_at_3_27_54_PM__1_;
+			this.btnInicio.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.btnInicio.Location = new System.Drawing.Point(13, 112);
+			this.btnInicio.Name = "btnInicio";
+			this.btnInicio.Size = new System.Drawing.Size(123, 76);
+			this.btnInicio.TabIndex = 2;
+			this.btnInicio.UseVisualStyleBackColor = true;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = global::pryEquipoII.Properties.Resources.WhatsApp_Image_2026_06_22_at_3_27_53_PM__1_;
+			this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(1370, 105);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::pryEquipoII.Properties.Resources.WhatsApp_Image_2026_06_22_at_3_27_53_PM;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 101);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(157, 648);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// frmInicio
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1370, 749);
+			this.Controls.Add(this.btnHistorialMedico);
+			this.Controls.Add(this.btnConsulta);
+			this.Controls.Add(this.btnAgendarCita);
+			this.Controls.Add(this.btnRegistroPacientes);
+			this.Controls.Add(this.btnInicio);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "frmInicio";
+			this.Text = "frmInicio";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+        }
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Button btnInicio;
+		private System.Windows.Forms.Button btnRegistroPacientes;
+		private System.Windows.Forms.Button btnAgendarCita;
+		private System.Windows.Forms.Button btnConsulta;
+		private System.Windows.Forms.Button btnHistorialMedico;
+	}
+}
